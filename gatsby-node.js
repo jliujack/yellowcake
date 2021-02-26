e01f6b90b66e1790b9635b9ee55b441d98bfeef9
刘杰 <120868827@qq.com>
2021-02-26 17:53:23 +0800

    feat: change about us node

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,7 +48,7 @@ exports.createPages = ({ actions, graphql }) => {
         createPage({
           // page slug set in md frontmatter
           path: page.node.fields.slug,
-          component: path.resolve(
+          'about-us': path.resolve(
             `src/templates/${String(page.node.frontmatter.template)}.js`
           ),
           // additional data can be passed via context
