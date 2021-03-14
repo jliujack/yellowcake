855b8a027f89a2a572425322a3ea8221fcca9007
刘杰 <jliujack@qtrade.com.cn>
2021-03-15 00:36:11 +0800

    feat: change form

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -23,12 +23,14 @@ class Form extends React.Component {
   handleSubmit = e => {
     e.preventDefault()
     if (this.state.disabled) return
-
-    const form = e.target
-    const data = serialize(form)
+    const myForm = document.getElementById('debby-form')
+    const formData = new FormData(myForm)
+    // formData.append('g-recaptcha-response', true)
     this.setState({ disabled: true })
-    fetch(form.action + '?' + stringify(data), {
-      method: 'POST'
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString()
     })
       .then(res => {
         if (res.ok) {
@@ -38,7 +40,7 @@ class Form extends React.Component {
         }
       })
       .then(() => {
-        form.reset()
+        myForm.reset()
         this.setState({
           alert: this.props.successMessage,
           disabled: false
@@ -62,12 +64,19 @@ class Form extends React.Component {
           <script src="https://www.google.com/recaptcha/api.js" />
         </Helmet>
         <form
+          id="debby-form"
+          className="Form"
           name={name}
-          method="POST"
-          data-netlify-recaptcha="true"
+          action={action}
+          onSubmit={this.handleSubmit}
           data-netlify="true"
+          netlify-recaptcha="true"
+          data-netlify-recaptcha="true"
         >
-          <p>
+          {this.state.alert && (
+            <div className="Form--Alert">{this.state.alert}</div>
+          )}
+          <div className="Form--Group">
             <label className="Form--Label">
               <input
                 className="Form--Input Form--InputText"
@@ -78,32 +87,87 @@ class Form extends React.Component {
               />
               <span>Name</span>
             </label>
-          </p>
-          <p>
-            <label className="Form--Label">
+            {/* <label className="Form--Label">
               <input
                 className="Form--Input Form--InputText"
-                type="email"
-                placeholder="Email"
-                name="emailAddress"
+                type="text"
+                placeholder="Lastname"
+                name="lastname"
                 required
               />
-              <span>Email address</span>
-            </label>
-          </p>
-          <p>
-            <label className="Form--Label">
-              <textarea
-                className="Form--Input Form--Textarea Form--InputText"
-                placeholder="Message"
-                name="message"
-                rows="10"
-                required
+              <span>Lastname</span>
+            </label> */}
+          </div>
+          {/* <fieldset>
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="male"
+                defaultChecked
               />
-              <span>Message</span>
+              <span>Male</span>
             </label>
-          </p>
-
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="female"
+              />
+              <span>Female</span>
+            </label>
+          </fieldset> */}
+          <label className="Form--Label">
+            <input
+              className="Form--Input Form--InputText"
+              type="email"
+              placeholder="Email"
+              name="emailAddress"
+              required
+            />
+            <span>Email address</span>
+          </label>
+          {/* <label className="Form--Label has-arrow">
+            <select
+              className="Form--Input Form--Select"
+              name="type"
+              defaultValue="Type of Enquiry"
+              required
+            >
+              <option disabled hidden>
+                Type of Enquiry
+              </option>
+              <option>Need to know more</option>
+              <option>Found a bug</option>
+              <option>Want to say hello</option>
+            </select>
+          </label> */}
+          <label className="Form--Label">
+            <textarea
+              className="Form--Input Form--Textarea Form--InputText"
+              placeholder="Message"
+              name="message"
+              rows="10"
+              required
+            />
+            <span>Message</span>
+          </label>
+          {/* <label className="Form--Label Form-Checkbox">
+            <input
+              className="Form--Input Form--Textarea Form--CheckboxInput"
+              name="newsletter"
+              type="checkbox"
+            />
+            <span>Get news updates</span>
+          </label> */}
+          <div
+            className="g-recaptcha"
+            data-sitekey="6Lde-34aAAAAAMFz98cb_AXTebEvqCjyL5KFxFDx"
+          />
+          {!!subject && <input type="hidden" name="subject" value={subject} />}
+          <input type="hidden" name="form-name" value={name} />
           <input
             className="Button Form--SubmitButton"
             type="submit"
