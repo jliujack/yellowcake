16166f7dd6acc4f7eadd526d24102c225225f550
刘杰 <jliujack@qtrade.com.cn>
2021-03-14 23:59:01 +0800

    feat: change form

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -62,18 +62,12 @@ class Form extends React.Component {
           <script src="https://www.google.com/recaptcha/api.js" />
         </Helmet>
         <form
-          className="Form"
           name={name}
-          action={action}
-          onSubmit={this.handleSubmit}
-          data-netlify={true}
-          netlify-recaptcha={true}
-          data-netlify-recaptcha={true}
+          method="POST"
+          data-netlify-recaptcha="true"
+          data-netlify="true"
         >
-          {this.state.alert && (
-            <div className="Form--Alert">{this.state.alert}</div>
-          )}
-          <div className="Form--Group">
+          <p>
             <label className="Form--Label">
               <input
                 className="Form--Input Form--InputText"
@@ -84,87 +78,32 @@ class Form extends React.Component {
               />
               <span>Name</span>
             </label>
-            {/* <label className="Form--Label">
+          </p>
+          <p>
+            <label className="Form--Label">
               <input
                 className="Form--Input Form--InputText"
-                type="text"
-                placeholder="Lastname"
-                name="lastname"
+                type="email"
+                placeholder="Email"
+                name="emailAddress"
                 required
               />
-              <span>Lastname</span>
-            </label> */}
-          </div>
-          {/* <fieldset>
-            <label className="Form--Label Form--Radio">
-              <input
-                className="Form--RadioInput"
-                type="radio"
-                name="gender"
-                value="male"
-                defaultChecked
-              />
-              <span>Male</span>
+              <span>Email address</span>
             </label>
-            <label className="Form--Label Form--Radio">
-              <input
-                className="Form--RadioInput"
-                type="radio"
-                name="gender"
-                value="female"
+          </p>
+          <p>
+            <label className="Form--Label">
+              <textarea
+                className="Form--Input Form--Textarea Form--InputText"
+                placeholder="Message"
+                name="message"
+                rows="10"
+                required
               />
-              <span>Female</span>
+              <span>Message</span>
             </label>
-          </fieldset> */}
-          <label className="Form--Label">
-            <input
-              className="Form--Input Form--InputText"
-              type="email"
-              placeholder="Email"
-              name="emailAddress"
-              required
-            />
-            <span>Email address</span>
-          </label>
-          {/* <label className="Form--Label has-arrow">
-            <select
-              className="Form--Input Form--Select"
-              name="type"
-              defaultValue="Type of Enquiry"
-              required
-            >
-              <option disabled hidden>
-                Type of Enquiry
-              </option>
-              <option>Need to know more</option>
-              <option>Found a bug</option>
-              <option>Want to say hello</option>
-            </select>
-          </label> */}
-          <label className="Form--Label">
-            <textarea
-              className="Form--Input Form--Textarea Form--InputText"
-              placeholder="Message"
-              name="message"
-              rows="10"
-              required
-            />
-            <span>Message</span>
-          </label>
-          {/* <label className="Form--Label Form-Checkbox">
-            <input
-              className="Form--Input Form--Textarea Form--CheckboxInput"
-              name="newsletter"
-              type="checkbox"
-            />
-            <span>Get news updates</span>
-          </label> */}
-          <div
-            className="g-recaptcha"
-            data-sitekey="6Lde-34aAAAAAMFz98cb_AXTebEvqCjyL5KFxFDx"
-          />
-          {!!subject && <input type="hidden" name="subject" value={subject} />}
-          <input type="hidden" name="form-name" value={name} />
+          </p>
+
           <input
             className="Button Form--SubmitButton"
             type="submit"
