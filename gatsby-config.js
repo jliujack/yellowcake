b3288991f36266e27adefe7c354b06215f51fe71
刘杰 <jliujack@qtrade.com.cn>
2021-07-07 23:06:49 +0800

    choro: change meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Legalandwise',
+    siteUrl: 'https://www.legalandwise.com/'
   },
   plugins: [
     {
